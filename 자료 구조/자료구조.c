69266e7dfe6622662a0cb567af6781e437c8cc19
nrbns357 <gusgh357@gmail.com>
2021-09-06 19:52:09 +0900

    8월 30일 팩토리얼

--- 자료 구조/자료구조.c
+++ 자료 구조/자료구조.c
@@ -82,3 +82,17 @@
 //	selectionSort(a, n);
 //	output(a, n);
 //}
+
+//8¿ù 30ÀÏ
+//long factorial(long c)
+//{
+//	return((c == 0) ? 1 : c * factorial(c - 1));
+//}
+//
+//int main() {
+//	long cnt = 0, n;
+//	for (cnt = 0; cnt <= 10; cnt++) {
+//		n = factorial(cnt);
+//		printf("%d!=%d\n", cnt, n);
+//	}
+//}
