d94300128f65ed397d70919c6e80f7b2c5fe11f0
nrbns357 <gusgh357@gmail.com>
2021-09-15 11:06:26 +0900

    쪽지시험 공부

--- 자료 구조/버블정렬.c
+++ 자료 구조/버블정렬.c
@@ -43,65 +43,87 @@
 
 
 
-#define SIZE 5	//스택에 넣을 수 있는 최대 사이즈
-void push(int); //스택에서 데이터 한 개 삽입
-void pop();		//스택에서 데이터 한 개 삭제
-void display(); //스택에 있는 데이터 보여주기
+//#define SIZE 5	//스택에 넣을 수 있는 최대 사이즈
+//void push(int); //스택에서 데이터 한 개 삽입
+//void pop();		//스택에서 데이터 한 개 삭제
+//void display(); //스택에 있는 데이터 보여주기
+//int stack[SIZE];
+//int top = -1; //스택에 저장할 위치
+
+//int main() {
+//	int val, choice;
+//	printf("\n----Menu----\n");
+//	while (1) {
+//		printf("1.push   2.pop   3.display   4.exit\n");
+//		printf("Choice : ");
+//		scanf("%d", &choice);
+//		switch (choice) {
+//		case 1: //스택에 push
+//			printf("데이터를 입력하세요");
+//			scanf("%d", &val);
+//			push(val);
+//			break;
+//
+//		case 2: // 스택에서 pop
+//			pop();
+//			break;
+//		case 3: // 스택 내용 display
+//			display();
+//			break;
+//		case 4:  // 프로그램 종료 exit
+//			return 0;
+//		default:
+//			printf("다시 입력하세요.");
+//		}
+//	}
+//	return 0;
+//}
+//
+//void push(int val) { //쪽지시험,  암기 필요
+//	if (top == SIZE - 1) //top >=SIZE-1
+//		printf("스택이 가득참\n");
+//	else
+//		stack[++top] = val; // 배열층들을 올라가면서 값을 넣어주기 위해 [++top]을 해준다.
+//}
+//
+//void pop() { //쪽지시험, 암기 필요
+//	if (top == -1)
+//		printf("스택이 비었음\n");
+//	else
+//		printf("%d가 삭제됨\n", stack[top--]); // 배열층들을 내려가면서 값들을 빼주는데 [--top]을 해주면 바로 내려가서 그 층의 값을 빼주기 때문에 늦게 [top--]을 해줌으로써 값을 빼고 내려간다. 
+//}
+//
+//void display() {
+//	if (top == -1)
+//		printf("스택이 비었음\n");
+//	else
+//		for (int i = top; i >= 0; i--)
+//			printf("%4d", stack[i]);
+//	printf("\n");
+//}
+
+
+#define SIZE 5
+void pop();
+void push(int);
 int stack[SIZE];
-int top = -1; //스택에 저장할 위치
+int top = -1;
 
-int main() {
-	int val, choice;
-	printf("\n----Menu----\n");
-	while (1) {
-		printf("1.push   2.pop   3.display   4.exit\n");
-		printf("Choice : ");
-		scanf("%d", &choice);
-		switch (choice) {
-		case 1: //스택에 push
-			printf("데이터를 입력하세요");
-			scanf("%d", &val);
-			push(val);
-			break;
-
-		case 2: // 스택에서 pop
-			pop();
-			break;
-		case 3: // 스택 내용 display
-			display();
-			break;
-		case 4:  // 프로그램 종료 exit
-			return 0;
-		default:
-			printf("다시 입력하세요.");
-		}
+void push(int val) {
+	if (top == SIZE - 1) {
+		printf("스택이 가득참");
 	}
-	return 0;
+	else {
+		stack[++top] = val;
+	}
 }
 
-void push(int val) { //쪽지시험,  암기 필요
-	if (top == SIZE - 1) //top >=SIZE-1
-		printf("스택이 가득참\n");
-	else
-		stack[++top] = val; // 배열층들을 올라가면서 값을 넣어주기 위해 [++top]을 해준다.
-}
-
-void pop() { //쪽지시험, 암기 필요
+void pop() {
 	if (top == -1)
-		printf("스택이 비었음\n");
+		printf("스택이 비었음");
 	else
-		printf("%d가 삭제됨\n", stack[top--]); // 배열층들을 내려가면서 값들을 빼주는데 [--top]을 해주면 바로 내려가서 그 층의 값을 빼주기 때문에 늦게 [top--]을 해줌으로써 값을 빼고 내려간다. 
+		printf("%d가 삭제됨", stack[top--]);
 }
-
-void display() {
-	if (top == -1)
-		printf("스택이 비었음\n");
-	else
-		for (int i = top; i >= 0; i--)
-			printf("%4d", stack[i]);
-	printf("\n");
-}
-
 
 
 
