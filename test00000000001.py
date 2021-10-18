bc9c66242e144ea66698fe4eeb642fb2470ef141
금현호 <80746320+nrbns357@users.noreply.github.com>
2021-10-19 08:51:50 +0900

    자료구조 파이썬 수업

---
+++ test00000000001.py
@@ -0,0 +1,48 @@
+#print(2**100)
+#print(2**5)
+#실행 단축키: shift + f10
+#소스코드 바뀔 때 단축키 alt + shift + f10
+#주석은 '#'으로 한다.
+
+
+
+
+# from tkinter import *
+# w=Tk()
+# w.title('gusgh')
+# w.geometry('400X200')
+# w.mainloop()
+
+# print(10>5) #Ture
+# print(10<3) #false
+# print(10>5 or 10<3) #ture
+# print(10>5 and 10<3) #false
+# print(not True) #false
+# print(not True and True)
+# print(not True or True)
+
+
+# name=input('이름을 입력하세요 : ')
+# print("my name is",name)
+# print("my name is"+name)
+
+
+a=input('a=' ) #a=int(input('a='))
+b=input('b=')
+a=int(a)
+b=int(b)
+print('-'*30)
+print(a+b)
+print('-'*30)
+
+print(a+b)
+
+
+# 두 숫자를 입력 받아 사칙연산하는 프로그램을 만드시오
+
+a=int(input())
+b=int(input())
+print('a+b',a+b)
+print('a-b',a-b)
+print('a*b',a*b)
+print('a/b',a/b)
---
+++ text0002.py
@@ -0,0 +1,16 @@
+# def Hello():
+#     print('Hello')
+#     print('OK')
+
+#파이썬에는 중괄호 {}로 함수 범위를 구분하지 않는다.
+#대신 들여쓰기(indentation)로 코드의 각 영역을 구분한다.
+
+# Hello()
+
+
+def Hello(name):
+    print('Hi',name)
+
+def Hello1(name):
+    print(f'Hi {name}, I am Korean')
+Hello1('OK')
