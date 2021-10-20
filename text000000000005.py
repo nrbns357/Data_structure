aa8cfd3665f574296aea7144e0e5caf1e29621bf
금현호 <80746320+nrbns357@users.noreply.github.com>
2021-10-20 15:15:13 +0900

    to day learned.

---
+++ text000000000005.py
@@ -0,0 +1,53 @@
+# li=[]
+# li.append('BTS')
+# li.append('Black Pink')
+# print(li)
+# print(li[1])
+#
+#
+# #가수 세 명을 입력 받기
+# for i in range(0,3,1):
+#     li.append(input('가수 이름을 쓰세요 : '))
+#
+# print('-' * 20)
+# print(li)
+# print(f'리스트 개수 {len(li)}')
+#
+
+#팩토리얼 구현
+#5! = 120
+
+# num = int(input("자연수 입력 : "))
+# fac = 1 #초기값 설정
+#
+# for i in range(1, num +1):
+#     fac = fac * i
+# print(f"{num} 팩토리얼은 {fac}입니다.")
+
+# import random
+# a1="오늘도 행복하세요"
+# a2 = "폭염 주의하세요"
+# a3="오늘 로또를 사세요"
+# a4="당신은 천재에요"
+# a5="영화 무료 쿠폰을 보내드립니다."
+# print(":::오늘의 운세:::")
+# input("엔터를 누르면 시작됩니다.")
+# c=random.randint(1,5)
+# if c==1:
+#     d=a1
+# elif c==2:
+#     d=a2
+# elif c==3:
+#     d=a3
+# elif c==4:
+#     d=a4
+# else:
+#     d=a5
+#
+# print('------------')
+# print(d)
+# print('------------')
+
+nation={'한국':'서울', '중국':'북경', '일본':'동경'}
+n=input('나라 이름 입력 : ')
+print(nation[n])
---
+++ text0000004.py
@@ -0,0 +1,34 @@
+# #print(d.hour)
+# #print(d.ctime())
+# #print(d)
+#
+# # import calendar
+# # import datetime
+# # d=datetime.datetime.now()
+# # print(calendar.month(d.year, d.month))
+#
+#
+# import random
+# arr = list() #arr을 리스트로 선언
+# print(arr) # []가 출력, 리스트를 의미
+#
+# for i in range(10): #10회 반복
+#     arr.append(random.randint(1,100))
+#     #arr리스트에 랜덤한 값을 넣는다.
+#
+# print('*' * 40)
+# print(arr)
+# print('*' * 40)
+#
+#
+#
+# # arr에 있는 데이터 중 가장 큰 값을 출력하시오.
+# # print(max(arr))
+#
+# #리스트 정렬
+# arr.sort()
+# print(arr)
+#
+# # 리스트 두번째 값을 출력하시오.
+# print(f'리스트 두번째 값 : {arr[1]}')
+#
